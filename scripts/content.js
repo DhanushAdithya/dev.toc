f3a16270a1a7037cc28f318fbf76d8c2380f2312
Dhanush Adithya <s.dhanushadithya@gmail.com>
2022-12-30 20:03:19 +0530

    fix: resolve route change issue
    - Fixed the issue where the extension was not being triggered for client side route changes.
    
    - Added a `MutationObserver` to detect changes to the `document.body` object and trigger the extension when necessary.
    
    - This fix should ensure that the extension works correctly and consistently when the route changes.

--- scripts/content.js
+++ scripts/content.js
@@ -1,87 +1,108 @@
 const $ = document.querySelector.bind(document);
 const create = document.createElement.bind(document);
 
-const articleBody = $("#article-body");
-const articleHeadings = [...articleBody.children].filter(e =>
-	/h[1-6]/g.test(e.localName)
-);
-
-const tocDetails = create("details");
-tocDetails.classList.add("devtoc__details");
-
-const tocSummary = create("summary");
-tocSummary.textContent = "Table of Contents";
-tocSummary.classList.add("devtoc__summary");
-
-const tocList = create("ul");
-tocList.classList.add("devtoc__ul");
-
-const titleItem = create("li");
-const titleAnchor = create("a");
-titleAnchor.href = "#main-title";
-titleAnchor.textContent = $(
-	".crayons-article__header__meta h1"
-).textContent.trim();
-
-titleAnchor.addEventListener("click", e => {
-	e.preventDefault();
-	tocDetails.open = false;
-	window.scrollTo({
-		top: 0,
-		behavior: "smooth",
-	});
-});
-
-titleItem.append(titleAnchor);
-tocList.append(titleItem);
-
-tocDetails.append(tocSummary);
-tocDetails.append(tocList);
-
-if (articleHeadings.length) {
-	const headingList = create("ul");
-	let currentList = headingList;
-	let currentLevel = Math.min(
-		...articleHeadings.map(e => +e.localName.slice(1))
+const addToC = () => {
+	const articleBody = $("#article-body");
+	const articleHeadings = [...articleBody.children].filter(e =>
+		/h[1-6]/g.test(e.localName)
 	);
 
-	for (const heading of articleHeadings) {
-		console.log("hit", heading.localName.slice(1));
-		const level = +heading.localName.slice(1);
+	const tocDetails = create("details");
+	tocDetails.classList.add("devtoc__details");
 
-		if (level > currentLevel) {
-			const newList = create("ul");
-			if (currentList.lastElementChild) {
-				currentList.lastElementChild.append(newList);
-			} else {
-				currentList.append(newList);
-			}
-			currentList = newList;
-			currentLevel = level;
-		} else if (level < currentLevel) {
-			currentList = currentList.parentElement;
-			currentLevel = level;
-		}
+	const tocSummary = create("summary");
+	tocSummary.textContent = "Table of Contents";
+	tocSummary.classList.add("devtoc__summary");
 
-		const headingItem = create("li");
-		const headingLink = create("a");
-		headingLink.href = [...heading.children].at(0).href;
-		headingLink.textContent = heading.textContent.trim();
+	const tocList = create("ul");
+	tocList.classList.add("devtoc__ul");
 
-		headingLink.addEventListener("click", e => {
-			e.preventDefault();
-			tocDetails.open = false;
-			const headingTop = heading.offsetTop;
-			window.scrollTo({
-				top: headingTop - 35,
-				behavior: "smooth",
-			});
+	const titleItem = create("li");
+	const titleAnchor = create("a");
+	titleAnchor.href = "#main-title";
+	titleAnchor.textContent = $(
+		".crayons-article__header__meta h1"
+	).textContent.trim();
+
+	titleAnchor.addEventListener("click", e => {
+		e.preventDefault();
+		tocDetails.open = false;
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
 		});
+	});
 
-		headingItem.append(headingLink);
-		currentList.append(headingItem);
+	titleItem.append(titleAnchor);
+	tocList.append(titleItem);
+
+	tocDetails.append(tocSummary);
+	tocDetails.append(tocList);
+
+	if (articleHeadings.length) {
+		const headingList = create("ul");
+		let currentList = headingList;
+		let currentLevel = Math.min(
+			...articleHeadings.map(e => +e.localName.slice(1))
+		);
+
+		for (const heading of articleHeadings) {
+			const level = +heading.localName.slice(1);
+
+			if (level > currentLevel) {
+				const newList = create("ul");
+				if (currentList.lastElementChild) {
+					currentList.lastElementChild.append(newList);
+				} else {
+					currentList.append(newList);
+				}
+				currentList = newList;
+				currentLevel = level;
+			} else if (level < currentLevel) {
+				currentList = currentList.parentElement;
+				currentLevel = level;
+			}
+
+			const headingItem = create("li");
+			const headingLink = create("a");
+			headingLink.href = [...heading.children].at(0).href;
+			headingLink.textContent = heading.textContent.trim();
+
+			headingLink.addEventListener("click", e => {
+				e.preventDefault();
+				tocDetails.open = false;
+				const headingTop = heading.offsetTop;
+				window.scrollTo({
+					top: headingTop - 35,
+					behavior: "smooth",
+				});
+			});
+
+			headingItem.append(headingLink);
+			currentList.append(headingItem);
+		}
+		tocList.append(headingList);
 	}
-	tocList.append(headingList);
-}
 
-articleBody.prepend(tocDetails);
+	articleBody.prepend(tocDetails);
+};
+
+const targetNode = document.body;
+const observer = new MutationObserver(mutations => {
+	if (mutations[0].target === document.body) {
+		const articleBody = $("#article-body");
+		if (articleBody) {
+			addToC();
+		}
+	}
+});
+const unobserve = () => {
+	observer.disconnect();
+};
+
+const loadExtension = () => {
+	if ($("#article-body")) addToC();
+	observer.observe(targetNode, { childList: true });
+};
+
+loadExtension();
